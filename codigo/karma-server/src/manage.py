484f4801c09ad5b2212847a6fe62721b86166787
Miguel Núñez Díaz-Montes <mnunezdm@gmail.com>
2017-06-24 19:06:07 +0200

    Updated requirements ; Imports in manager optimized

--- codigo/karma-server/src/manage.py
+++ codigo/karma-server/src/manage.py
@@ -1,9 +1,9 @@
-from flask_script import Manager, Shell, Server
-from app import create_app, start_server
+''' Manage module, provides all the interaction with the server '''
 from flask_migrate import MigrateCommand
-from debugger import init_terminal_colors
+from flask_script import Manager, Shell
 
-from testers import run_all_tests
+from app import create_app, start_server
+from debugger import init_terminal_colors
 
 init_terminal_colors()
 app = create_app()
@@ -15,6 +15,7 @@ manager.add_command("db", MigrateCommand)
 @manager.command
 def runtests():
     ''' Runs all the configured tests '''
+    from testers import run_all_tests
     run_all_tests()
 
 @manager.command
